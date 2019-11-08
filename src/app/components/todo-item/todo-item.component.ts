e06219bdba7e09f9b1e832a391fb026f8093ce51
Yusrizall <sukroprozas38@gmail.com>
2019-11-08 10:51:30 +0700

    commit kedua, memperbaiki bug pada todo-item.comoponent.ts

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 import { TodoService } from '../../services/todo.service';
 
-import { Todo } from '../models/Todo';
-
 @Component({
   selector: 'app-todo-item',
   templateUrl: './todo-item.component.html',
